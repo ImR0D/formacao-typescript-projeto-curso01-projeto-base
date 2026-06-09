314c72c2586f1a9dc9ce2f3dba77a4e5f93feb47
Rodrigo Ferreira <rodrigo.ferreira@pca.com.br>
2026-06-09 16:08:24 -0300

    feat: compilação do arquivo balance-component.ts

--- dist/scripts/balance-component.js
+++ dist/scripts/balance-component.js
@@ -1,1 +1,7 @@
 "use strict";
+let saldo = 3000;
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = saldo.toString();
+}
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
